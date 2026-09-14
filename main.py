c727b9512cbb3bad1ecdf2085bcbd45cebbe1c7e
MoRezaGholami <mohammadrezagh748@gmail.com>
2026-09-14 16:22:52 +0330

    start working on main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import logging
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+logging.basicConfig(
+    level=logging.INFO , 
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+logger = logging.getLogger("agent_app")
+
+
+
